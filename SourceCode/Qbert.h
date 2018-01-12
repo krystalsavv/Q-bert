e9f3e4eab958fc27679fde53dcdec105757faf32
tpaslou <csd3222@csd.uoc.gr>
2018-01-12 20:20:24 +0200

    Movement With Animation

--- SourceCode/Qbert.h
+++ SourceCode/Qbert.h
@@ -9,6 +9,7 @@ class Qbert {
 	Sprite *sprite;
 	int currRow;
 	int currCol;
+	std::list<PathEntry> path;
 public:
 	void Create(int x, int y) {
 		AnimationFilm* film = GetFilm();
@@ -39,22 +40,133 @@ public:
 	void moveDownRight() {
 		currRow++;
 		currCol++;
-		sprite->Move(50, 75);
+		PathEntry *p1 = new PathEntry(0, 0, 0, 70);
+		
+		PathEntry *p4 = new PathEntry(0,0, 1,70);
+		PathEntry *p5 = new PathEntry(0, -30, 1,70);
+		PathEntry *p6 = new PathEntry(40, 0, 0, 70);
+
+		PathEntry *p2 = new PathEntry(10,105,0,50);
+		PathEntry *p3 = new PathEntry(0, 0,0,0);
+		path.push_back(*p1);
+		path.push_back(*p4);
+		path.push_back(*p5);
+		path.push_back(*p6);
+		path.push_back(*p2);
+		path.push_back(*p3);
+
+		
+
+		MovingPathAnimation* qbertAnimation = new MovingPathAnimation(path, "qbert_Animation1");
+		MovingPathAnimator* qbertAnimator = new MovingPathAnimator(sprite, qbertAnimation);
+		qbertAnimator->Start(game->GetGameTime());
+		
+		path.clear();
+		path.push_back(*p1);
+		
+		
+		
+
+		
+		
+		//sprite->Move(50, 75);
+		
+
+
 	}
 	void moveDownLeft() {
 		currRow++;
-		sprite->Move(-50, 75);
+		//sprite->Move(-50, 75);
+		
+		PathEntry *p1 = new PathEntry(0, 0, 2, 70);
+		
+		PathEntry *p4 = new PathEntry(0,0, 3,70);
+		PathEntry *p5 = new PathEntry(0, -30, 3,70);
+		PathEntry *p6 = new PathEntry(-40, 0, 2, 70);
+
+		PathEntry *p2 = new PathEntry(-10,105,2,50);
+		PathEntry *p3 = new PathEntry(0, 0,2,0);
+		path.push_back(*p1);
+		path.push_back(*p4);
+		path.push_back(*p5);
+		path.push_back(*p6);
+		path.push_back(*p2);
+		path.push_back(*p3);
+
+		
+
+		MovingPathAnimation* qbertAnimation = new MovingPathAnimation(path, "qbert_Animation1");
+		MovingPathAnimator* qbertAnimator = new MovingPathAnimator(sprite, qbertAnimation);
+		qbertAnimator->Start(game->GetGameTime());
+		
+		path.clear();
+		path.push_back(*p1);
+		
+		
 		
 	}
 	void moveUpRight() {
 		currRow--;
-		sprite->Move(50, -75);
+		//sprite->Move(50, -75);
+		PathEntry *p1 = new PathEntry(0, 0, 0, 70);
+
+		PathEntry *p4 = new PathEntry(0, 0, 1, 70);
+		PathEntry *p5 = new PathEntry(0, -20, 1, 70);
+		PathEntry *p6 = new PathEntry(0,-50, 1, 70);
+
+		PathEntry *p2 = new PathEntry(50, -5, 0, 50);
+		PathEntry *p3 = new PathEntry(0, 0, 0, 0);
+		path.push_back(*p1);
+		path.push_back(*p4);
+		path.push_back(*p5);
+		path.push_back(*p6);
+		path.push_back(*p2);
+		path.push_back(*p3);
+
+
+
+		MovingPathAnimation* qbertAnimation = new MovingPathAnimation(path, "qbert_Animation1");
+		MovingPathAnimator* qbertAnimator = new MovingPathAnimator(sprite, qbertAnimation);
+		qbertAnimator->Start(game->GetGameTime());
+
+		path.clear();
+		path.push_back(*p1);
+
+
+
+
 
 	}
 	void moveUpLeft() {
 		currRow--;
 		currCol--;
-		sprite->Move(-50, -75);
+		//sprite->Move(-50, -75);
+		PathEntry *p1 = new PathEntry(0, 0, 2, 70);
+
+		PathEntry *p4 = new PathEntry(0, 0, 3, 70);
+		PathEntry *p5 = new PathEntry(0, -20, 3, 70);
+		PathEntry *p6 = new PathEntry(0, -50, 3, 70);
+
+		PathEntry *p2 = new PathEntry(-50, -5, 2, 50);
+		PathEntry *p3 = new PathEntry(0, 0, 2, 0);
+		path.push_back(*p1);
+		path.push_back(*p4);
+		path.push_back(*p5);
+		path.push_back(*p6);
+		path.push_back(*p2);
+		path.push_back(*p3);
+
+
+
+		MovingPathAnimation* qbertAnimation = new MovingPathAnimation(path, "qbert_Animation1");
+		MovingPathAnimator* qbertAnimator = new MovingPathAnimator(sprite, qbertAnimation);
+		qbertAnimator->Start(game->GetGameTime());
+
+		path.clear();
+		path.push_back(*p1);
+
+
+
 	}
 
 	void PrintPos() {
@@ -81,7 +193,7 @@ public:
 	}
 
 	void Animation() {   //!!!!!!!! gia test einai ayth h method
-		std::list<PathEntry> path;
+		/*std::list<PathEntry> path;
 		PathEntry *p;
 		for (int i = 0; i < 4; ++i) {
 			p = new PathEntry(-10, 0, i, 300);
@@ -91,6 +203,7 @@ public:
 		MovingPathAnimation* qbertAnimation = new MovingPathAnimation(path, "qbert_Animation1");  
 		MovingPathAnimator* qbertAnimator = new MovingPathAnimator(sprite, qbertAnimation);
 		qbertAnimator->Start(game->GetGameTime());
+	*/
 	}
 
 	Qbert(int x, int y){
